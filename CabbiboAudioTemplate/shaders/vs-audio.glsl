409cebb2fd55724f727735be0f00c87c8d989ac2
Christian Kokott <christian.kokott@gmail.com>
2015-05-03 19:46:40 +0200

    ufo update

--- CabbiboAudioTemplate/shaders/vs-audio.glsl
+++ CabbiboAudioTemplate/shaders/vs-audio.glsl
@@ -25,14 +25,12 @@ void main(){
   // Here we sample the audio from a texture that we will
   // be creating on the cpu
   vec4 audio = texture2D( t_audio , vec2( vAudioLookup , 0. ));
-
   vAudio = audio;
 
 
   // To visualize the audio, we will displace the position
   // by a value based on the audio, along the normal
   vec3 pos = position + .1 * length( audio ) * normal;
-  
   // Use this position to get the final position
   gl_Position = projectionMatrix * modelViewMatrix * vec4( pos , 1.);
 
